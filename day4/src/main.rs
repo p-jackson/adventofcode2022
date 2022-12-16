4b49e3f7c0e3e8f98d0ce53f4717f0ee8f71b428
Philip Jackson <p-jackson@live.com>
2022-12-17 01:02:56 +1300

    day 4 star 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         .lines()
         .filter_map(|line_result| line_result.ok())
         .filter_map(|line| parse(&line))
-        .filter(fully_overlaps)
+        .filter(partial_overlaps)
         .count();
 
     println!("{}", count);
@@ -29,12 +29,8 @@ fn parse_part(range: &str) -> Option<(u32, u32)> {
     Some((small, big))
 }
 
-fn fully_overlaps(partners: &((u32, u32), (u32, u32))) -> bool {
-    compare_part(partners.0, partners.1) || compare_part(partners.1, partners.0)
-}
-
-fn compare_part(first: (u32, u32), second: (u32, u32)) -> bool {
-    first.0 <= second.0 && first.1 >= second.1
+fn partial_overlaps(partners: &((u32, u32), (u32, u32))) -> bool {
+    partners.0 .1 >= partners.1 .0 && partners.0 .0 <= partners.1 .1
 }
 
 #[cfg(test)]
@@ -50,12 +46,12 @@ mod tests {
 
     #[test]
     fn test_fully_overlaps() {
-        assert!(fully_overlaps(&((1, 2), (1, 2))));
-        assert!(!fully_overlaps(&((2, 4), (6, 8))));
-        assert!(!fully_overlaps(&((2, 3), (4, 5))));
-        assert!(!fully_overlaps(&((5, 7), (7, 9))));
-        assert!(fully_overlaps(&((2, 8), (3, 7))));
-        assert!(fully_overlaps(&((6, 6), (4, 6))));
-        assert!(!fully_overlaps(&((2, 6), (4, 8))));
+        assert!(partial_overlaps(&((1, 2), (1, 2))));
+        assert!(!partial_overlaps(&((2, 4), (6, 8))));
+        assert!(!partial_overlaps(&((2, 3), (4, 5))));
+        assert!(partial_overlaps(&((5, 7), (7, 9))));
+        assert!(partial_overlaps(&((2, 8), (3, 7))));
+        assert!(partial_overlaps(&((6, 6), (4, 6))));
+        assert!(partial_overlaps(&((2, 6), (4, 8))));
     }
 }
